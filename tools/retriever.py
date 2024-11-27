79df97a99504076429733725bcb99df1aa93bd07
paolodepa <paolo.depasquale@suse.com>
2024-11-27 11:01:39 +0100

    Remove retriever.py as it is no longer needed for the project

--- tools/retriever.py
+++
@@ -1,8 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(vector_file)
-collection = client.get_or_create_collection(name=collection_name)
-
-results = collection.query(
-    query_texts=[query_text]
-)
