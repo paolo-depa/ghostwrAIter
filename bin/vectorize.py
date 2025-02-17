eb8738729ed3e41cdaaf99d8e37bc2f614e4f65a
paolodepa <paolo.depasquale@suse.com>
2025-02-17 14:25:47 +0100

    feature: local embedding calculation moved from chromadb to fastembeddings wrapper

--- bin/vectorize.py
+++ bin/vectorize.py
@@ -6,11 +6,9 @@ import time
 import sys
 
 from langchain_ollama import OllamaEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from chromadb.utils import embedding_functions
-
+from langchain_community.embeddings.fastembed  import FastEmbedEmbeddings
 
 def parse_and_validate_args():
     parser = argparse.ArgumentParser(description="Vectorize text files in a directory using an embedding model.")
@@ -18,13 +16,11 @@ def parse_and_validate_args():
     parser.add_argument("--vector_dir", type=str, help="Path to the directory where the chromadb will be saved.")
     parser.add_argument("--ollama_url", type=str, help="URL for the Ollama service.")
     parser.add_argument("--ollama_embedding", type=str, help="Embedding model for the Ollama service.")
+    parser.add_argument("--ollama_num_gpus", type=int, default=0, help="Number of GPUs to use for the Ollama service.")
     # not working with the current version of langchain-community
     # parser.add_argument("-r", "--recursive", action="store_true", help="Scan the directory recursively.")
     parser.add_argument("--exclude", action="append", help="Paths to exclude, enquoted in single quotes. Can be used multiple times.")
     args = parser.parse_args()
-
-    if not args.directory:
-        args.directory = "."
 
     # Verify that the directory is readable
     if not os.access(args.directory, os.R_OK):
@@ -35,20 +31,21 @@ def parse_and_validate_args():
     if not args.vector_dir:
         args.vector_dir = os.path.join(args.directory, ".chroma.db")
 
-    # Check for ollama_url argument or read from config file
-    if not args.ollama_url or not args.ollama_embedding:
-        config_path = os.path.expanduser("~/.config/ghostwraiter/settings.json")
-        if os.path.exists(config_path):
-            try:
-                with open(config_path, 'r') as config_file:
-                    config = json.load(config_file)
-                    if not args.ollama_url:
-                        args.ollama_url = config.get("ollama.url")
-                    if not args.ollama_embedding:
-                        args.ollama_embedding = config.get("ollama.embedding")
-            except (IOError, json.JSONDecodeError):
-                print(f"Error reading configuration file {config_path}.", file=sys.stderr)
-                return None
+    # 
+    config_path = os.path.expanduser("~/.config/ghostwraiter/settings.json")
+    if os.path.exists(config_path):
+        try:
+            with open(config_path, 'r') as config_file:
+                config = json.load(config_file)
+                if not args.ollama_url:
+                    args.ollama_url = config.get("ollama.url")
+                if not args.ollama_embedding:
+                    args.ollama_embedding = config.get("ollama.embedding")
+                if not args.ollama_num_gpus:
+                    args.ollama_num_gpus = config.get("ollama.num_gpus")
+
+        except (IOError, json.JSONDecodeError):
+            print(f"Error reading configuration file {config_path}.", file=sys.stderr)
 
     # Add vector store base path to exclude paths
     if args.exclude is None:
@@ -106,9 +103,14 @@ def main():
     full_path = os.path.abspath(args.directory)
     collection_name = os.path.basename(full_path)
 
-    embedding_function = embedding_functions.DefaultEmbeddingFunction()
+    # Running locally on GUI needs root privileges and fastembed-gui installed
+    # embedding_function = FastEmbedEmbeddings(providers=["CUDAExecutionProvider"])
+    embedding_function = FastEmbedEmbeddings(batch_size=512, parallel=0)
     if args.ollama_url and args.ollama_embedding:
-        embedding_function = OllamaEmbeddings(base_url=args.ollama_url, model=args.ollama_embedding)
+        print(f"Embedding calculated at {args.ollama_url} with model {args.ollama_embedding}")
+        embedding_function = OllamaEmbeddings(base_url=args.ollama_url, model=args.ollama_embedding, num_gpu=args.ollama_num_gpus)
+    else:
+        print(f"Embedding calculated locally using FastEmbeddings")
     
     update_exclude_and_content(args)
 
@@ -132,21 +134,21 @@ def main():
         return
 
     try:
-         vector_store = Chroma(
-             collection_name=collection_name,
-             persist_directory=args.vector_dir,
-             embedding_function=embedding_function
-         )
+        vector_store = Chroma(
+            collection_name=collection_name,
+            persist_directory=args.vector_dir,
+            embedding_function=embedding_function
+        )
 
-         batch_size = 500
-         total_docs = len(splitted)
-         vector_start=time.time()
-         print(f"Starting vector creation at: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(vector_start))}")
-         for i in range(0, total_docs, batch_size):
-             batch = splitted[i:i + batch_size]
-             ids = vector_store.add_documents(batch)
-             vector_current = time.time()
-             print(f"Added {(i + len(ids)) } / {total_docs} chunks to vector store in {int(vector_current - vector_start)} seconds.")
+        batch_size = 500
+        total_docs = len(splitted)
+        vector_start=time.time()
+        print(f"Starting vector creation at: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(vector_start))}")
+        for i in range(0, total_docs, batch_size):
+            batch = splitted[i:i + batch_size]
+            ids = vector_store.add_documents(batch)
+            vector_current = time.time()
+            print(f"Added {(i + len(ids)) } / {total_docs} chunks to vector store in {int(vector_current - vector_start)} seconds.")
 
     except Exception as e:
         print(f"Error adding documents to vector store: {e}", file=sys.stderr)
